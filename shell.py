357e82be508bd4c5dd7074a0a9ccbb791370fd0a
val-ornelas <85901299+val-ornelas@users.noreply.github.com>
2021-07-06 14:37:11 -0600

    Update shell.py

--- shell.py
+++ shell.py
@@ -3,7 +3,7 @@ import basic
 while True:
     text = input('basic > ')
     print('text:', text)
-    result, error = basic.run(text)
+    result, error = basic.run('<stdin>', text)
 
     if error: print(error.as_string())
     else: print(result)
